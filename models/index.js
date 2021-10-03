c7d503a0a62416cc7511d343f0f62c007e741e27
Kabao Thao <tkabao@gmail.com>
2021-10-03 16:12:42 -0500

    update associations in index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,20 @@
-const User = require('./User');
+const User = require("./User");
+const GameRoom = require("./game_room");
 
-module.exports = { User };
+//create associations
+//users hasOne gameroom
+User.hasOne(GameRoom, {
+  foreignKey: "room_id",
+});
+
+//gameroom belongsTo user
+GameRoom.belongsTo(User, {
+  foreignKey: "room_id",
+});
+
+//gameroom hasmany user
+GameRoom.hasMany(User, {
+  foreignKey: "room_id",
+});
+
+module.exports = { User, GameRoom };
