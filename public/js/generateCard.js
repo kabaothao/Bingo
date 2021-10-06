1ccdda6f8766640837e506f102a0d2907ce015e3
Jesse Erickson <clerick44@hotmail.com>
2021-10-06 18:53:07 -0500

    migrated js to single file

--- public/js/generateCard.js
+++ public/js/generateCard.js
@@ -1,45 +1,57 @@
-//Generating a bingo card
-function genNum() {
-    $b = Math.floor((Math.random()) * (15 - 1)) + 1;
-    $i = Math.floor((Math.random()) * (30 - 16)) + 16;
-    $n = Math.floor((Math.random()) * (45 - 31)) + 31;
-    $g = Math.floor((Math.random()) * (60 - 46)) + 46;
-    $o = Math.floor((Math.random()) * (75 - 61)) + 61;
-  }
-  
-  function makeNum() {
-    genNum();
-    if ($y == 1) $num = $b;
-    if ($y == 2) $num = $i;
-    if ($y == 3) $num = $n;
-    if ($y == 4) $num = $g;
-    if ($y == 5) $num = $o;
-    $numList.push($num);
-    for ($c = 0; $c <= $count; $c++) {
-      if ($num != $numList[$c - 1]) {
-        $good = 1;
-      } else {
-        $good = 0;
-        makeNum();
-      }
-    }
-  }
-  $('button').click(function() {
-    $('.column').empty();
-    $numList = [];
-    $count = 1;
-    for ($x = 1; $x <= 5; $x++) {
-      for ($y = 1; $y <= 5; $y++) {
-        makeNum();
-        if ($good == 1) {
-          $('.' + $y).append("<div class='number col-" + $count + "'><span>" + $num + "</span></div>");
-        }
-        $count++;
-      }
-    }
-    $(".col-13").html("<span>FREE</span>");
-  
-    $('.number').click(function() {
-      $(this).html('<span>X</span>');
-    });
+// const cardNumbers = require("../js/generateCardNumbers");
+
+var cardNumbers = [];
+
+$("#bButton").click(() => {
+  // event.stopPropagation();
+  console.log("Im in buildCard");
+  // $("numberField").empty();
+  let cardNumbers = generateCardNumbers();
+  console.log(cardNumbers);
+  cardNumbers.forEach((e, index) => {
+    console.log(index);
+    $("#numberField1").append("<div class='number'>" + e + "</div>");
   });
+});
+
+// $(".number").click(function () {
+//   $(this).html("<span>X</span>");
+// });
+
+const generateCardNumbers = () => {
+  for (let i = 0; i < 5; i++) {
+    rowNumbers = [];
+
+    rowNumbers.push(Math.floor(Math.random() * (15 - 1)) + 1);
+    rowNumbers.push(Math.floor(Math.random() * (30 - 16)) + 16);
+    if (i === 2) {
+      rowNumbers.push("FREE");
+    } else {
+      rowNumbers.push(Math.floor(Math.random() * (45 - 31)) + 31);
+    }
+    rowNumbers.push(Math.floor(Math.random() * (60 - 46)) + 46);
+    rowNumbers.push(Math.floor(Math.random() * (75 - 61)) + 61);
+
+    isDuplicate(rowNumbers)
+      ? i--
+      : rowNumbers.forEach((e) => {
+          cardNumbers.push(e);
+        });
+    // console.log(cardNumbers);
+  }
+  return cardNumbers;
+};
+
+const isDuplicate = (rowNumbers) => {
+  for (let i = 0; i < rowNumbers.length; i++) {
+    const duplicateCheck = cardNumbers.some((e) => {
+      return e === rowNumbers[i];
+    });
+
+    if (duplicateCheck === true) {
+      return duplicateCheck;
+    }
+  }
+};
+
+// module.exports;
