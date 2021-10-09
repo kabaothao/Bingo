d33a7f9f8de2446f0ac78777d598a44fb2e684fc
Jesse Erickson <clerick44@hotmail.com>
2021-10-09 15:24:39 -0500

    added color changing for bingo cards

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 var cardNumbers = [];
 var allCards = [];
 
-$("#button").click(() => {
+$("#generateCardBtn").click(() => {
   // event.stopPropagation();
   console.log("Im in buildCard");
   $(".column").empty();
@@ -31,6 +31,9 @@ $("#button").click(() => {
           $("#I" + i + "2").append(
             "<div class='number col-" + index + "'><span>" + e + "</span></div>"
           );
+          $(".number").click(function () {
+            $(this).addClass("dobbed");
+          });
           break;
         case 2:
         case 7:
@@ -40,6 +43,9 @@ $("#button").click(() => {
           $("#N" + i + "3").append(
             "<div class='number col-" + index + "'><span>" + e + "</span></div>"
           );
+          $(".number").click(function () {
+            $(this).addClass("dobbed");
+          });
           break;
         case 3:
         case 8:
@@ -49,6 +55,9 @@ $("#button").click(() => {
           $("#G" + i + "4").append(
             "<div class='number col-" + index + "'><span>" + e + "</span></div>"
           );
+          $(".number").click(function () {
+            $(this).addClass("dobbed");
+          });
           break;
         case 4:
         case 9:
@@ -58,6 +67,9 @@ $("#button").click(() => {
           $("#O" + i + "5").append(
             "<div class='number col-" + index + "'><span>" + e + "</span></div>"
           );
+          $(".number").click(function () {
+            $(this).addClass("dobbed");
+          });
           break;
         default:
           console.log("At default");
@@ -101,6 +113,6 @@ const generateCardNumbers = () => {
     // console.log(cardNumbers);
   }
   allCards.push(cardNumbers);
-  console.log(all);
+  console.log(allCards);
   return cardNumbers;
 };
