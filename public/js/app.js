1187e00b0082a527335ab745b1967930838be71c
Alena Rehberger <alenarehberger92@gmail.com>
2021-10-10 21:47:33 -0500

    Pulling the changes

--- public/js/app.js
+++ public/js/app.js
@@ -122,7 +122,6 @@ let generateBingoCards = function () {
   }
   document.getElementById("bingo_cards").style.opacity = 1;
 };
-
 const isDuplicate = (rowNumbers) => {
   for (let i = 0; i < rowNumbers.length; i++) {
     const duplicateCheck = cardNumbers.some((e) => {
@@ -167,12 +166,11 @@ const generateCardNumbers = () => {
 let winner = false;
 
 
-form.addEventListener("submit", function (e) {
+chatSendBtn.addEventListener("click", function (e) {
   e.preventDefault();
   if (input.value) {
     socket.emit("chat message", input.value);
     input.value = "";
-    messages.style.display = "inherit";
   }
 });
 
@@ -180,16 +178,7 @@ socket.on("chat message", function (msg) {
   var item = document.createElement("li");
   item.textContent = msg;
   messages.appendChild(item);
-  messages.scrollTo(0, document.body.scrollHeight);
-  messages.style.display = "inherit";
-});
-
-closeBtn.addEventListener("click", function () {
-  messages.style.display = "none";
-});
-
-showComments.addEventListener("click", function () {
-  messages.style.display = "inherit";
+  window.scrollTo(0, document.body.scrollHeight);
 });
 
 socket.on("host", (data) => {
@@ -220,12 +209,22 @@ socket.on("beginGame", (data) => {
   }
 });
 
+// socket.on("current time", function (data) {
+//   console.log(data);
+//   console.log(Player.hasStarted);
+//   if (!Player.hasStarted) {
+//     Player.startTime = data;
+//     Player.hasStarted = true;
+//   }
+// });
+
 var Host = {
   isHost: "",
 
+  // socket.on("beginGame", onBeginGame),
+
   init: function () {
-    Host.timer(60);
-    while()
+    Host.timer(99);
   },
 
   timer: function (time) {
@@ -240,7 +239,7 @@ var Host = {
       } else {
         timerEl.textContent = "00:00";
         clearInterval(Host.timer);
-        Host.callBall();
+        // endGame();
       }
     }, 1000);
   },
